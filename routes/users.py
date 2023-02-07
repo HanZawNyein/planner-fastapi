cf35bd67c06f749cb0c6529711a8f82d66ad23cb
HanZawNyein <hanzawnyineonline@gmail.com>
2023-02-07 15:55:30 +0630

    user events and basic sigin/out

---
+++ routes/users.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, HTTPException, status
+from models.users import User, UserSignIn
+
+router = APIRouter(tags=['users'])
+
+users = {}
+
+
+@router.post("/signup")
+async def sign_new_user(data: User) -> dict:
+    if data.email in users:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with supplied username exists"
+        )
+    users[data.email] = data
+    return {"message": "User successfull registerd."}
+
+@router.post("/signin")
+async def sign_user_in(user:UserSignIn)->dict:
+    # print(user.email,users)
+    # print(users[user.email])
+    if user.email not in users:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Users does not exist."
+        )
+    if users[user.email].password != user.password:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Wrong Credentials Passed."
+        )
+    
+    return {"message":"User signed in Successfully."}
